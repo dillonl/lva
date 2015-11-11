482006599722cb21175a00a5322237482d35740b
dillonl <dlee@genetics.utah.edu>
2015-11-11 09:54:05 -0700

    changed some button functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+function loadSampleDataClicked() {
+	$('#load-sample-button').prop("disabled",true);
+}
+
 function loadSampleData() {
 	$.get('data/somatic.graphite.vcf', function (vcfString) {
 		var vcf = new VCF(vcfString);
