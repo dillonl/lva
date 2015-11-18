63bfa6375849d4dddf480506351985f341366a89
Dillon Lee <dillonl@sdcenter.utah.edu>
2015-11-18 10:08:46 -0700

    in the middle of the parallel coordinates plot

--- js/Variant.js
+++ js/Variant.js
@@ -4,3 +4,23 @@ var Variant = function (chromosome, position, referenceCount, alternateCounts) {
 	this.referenceCount = referenceCount;
 	this.alternateCounts = alternateCounts;
 }
+
+Variant.prototype.getAlternateAlleleFrequency = function () {
+	var maxAlternateCount = this.getMaxAlternateCount();
+	var totalCounts = this.referenceCount + maxAlternateCount;
+	return (totalCounts == 0) ? 0 : maxAlternateCount / totalCounts;
+}
+
+Variant.prototype.getReferenceAlleleFrequency = function() {
+	var maxAlternateCount = this.getMaxAlternateCount();
+	var totalCounts = this.referenceCount + maxAlternateCount;
+	return (totalCounts == 0) ? 0 : this.referenceCount / totalCounts;
+}
+
+Variant.prototype.getMaxAlternateCount = function () {
+	var maxAlternateCount = 0;
+	for (var i = 0; i < this.alternateCounts; ++i) {
+		maxAlternateCount = (maxAlternateCount < this.alternateCounts[i]) ? this.alternateCounts[i] : maxAlternateCount;
+	}
+	return maxAlternateCount;
+}
